92a2f0d1336b59426fd938a04950df9c6d1c8241
Gitroars <501sterror@gmail.com>
2022-06-17 22:36:28 +0700

    Save and Load

---
+++ Library/Assets/Scripts/Policy/PolicyHover.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolicyHover : MonoBehaviour
+{
+    public int policyId;
+    public GameObject policies;
+    public GameObject policyForDetail;
+    public GameObject cameraMovement;
+
+    private Policy[] allPolicies;
+    private Policy chosenPolicy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(policyId != 0)
+        {
+            allPolicies = policies.GetComponent<PolicyScript>().policies;
+            chosenPolicy = searchPolicy(allPolicies, policyId);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        if(cameraMovement.GetComponent<CameraMovement>().isActive == true)
+        {
+            policyForDetail.SetActive(true);
+            policyForDetail.GetComponent<PolicyDetail>().policy = chosenPolicy;
+        }
+    }
+
+    Policy searchPolicy(Policy[] allPolicies, int id)
+    {
+        foreach(var policy in allPolicies)
+        {
+            if(policy.id == id)
+            {
+                return policy;
+            }
+        }
+
+        return null;
+    }
+}
