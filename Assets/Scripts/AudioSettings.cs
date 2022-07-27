036f0b72dfc3eb98f4bcd0dca032b09c5b4be0d0
Gitroars <501sterror@gmail.com>
2022-07-27 09:14:06 +0700

    Audio Patch
    - Fix SFX at CreditsScene
    - Add Volume Integer Value Text Display at SettingsScene

--- Assets/Scripts/AudioSettings.cs
+++ Assets/Scripts/AudioSettings.cs
@@ -6,6 +6,7 @@ public class AudioSettings : MonoBehaviour
 {
     
     public Slider bgmSlider,sfxSlider;
+    public Text bgmText,sfxText;
     
     private void Awake() {
         LoadVolume();
@@ -24,14 +25,29 @@ public class AudioSettings : MonoBehaviour
     void LoadVolume(){
         bgmSlider.value = PlayerPrefs.GetFloat("bgmVolume",0.5f);
         sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume",0.5f);
+        UpdateAudioDisplay();
     }
     public void UpdateBGMVolume(){
         float bgmValue = bgmSlider.value;
         PlayerPrefs.SetFloat("bgmVolume", bgmValue);
+        UpdateBGMDisplay();
     }
     public void UpdateSFXVolume(){
         float sfxValue = sfxSlider.value;
         PlayerPrefs.SetFloat("sfxVolume", sfxValue);
+        UpdateSFXDisplay();
+    }
+    void UpdateBGMDisplay(){
+        int tempBgmValue = (int)(bgmSlider.value*100);
+        bgmText.text = tempBgmValue.ToString();
+    }
+    void UpdateSFXDisplay(){
+        int tempSfxValue = (int)(sfxSlider.value*100);
+        sfxText.text = tempSfxValue.ToString();
+    }
+    void UpdateAudioDisplay(){
+        UpdateBGMDisplay();
+        UpdateSFXDisplay();
     }
     
 }
